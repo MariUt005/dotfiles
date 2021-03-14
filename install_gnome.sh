a68e9787ff8f67cb6c53f70eda3e2adfe6460fb1
MariUt005 <60814898+MariUt005@users.noreply.github.com>
2021-03-14 10:03:17 +0300

    Delete install_gnome.sh

--- install_gnome.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S gnome gnome-extra
-systemctl enable gdm.service --force
-sudo pacman -S manjaro-gnome-assets manjaro-gdm-theme manjaro-settings-manager
-sudo useradd -mG lp,network,power,sys,wheel <username>
-sudo passwd <username>
